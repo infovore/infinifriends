f5bd05b954480b944d20a0694fe378998b0c8189
Tom Armitage <tom@infovore.org>
2013-11-22 09:19:55 +0000

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Infinifriends::Application.config.secret_key_base = '4cba9ac65484e8b0d5786ce573a565c06cdcff25eebbe7c87d48e110684e4f243895a9f6dde33fb5a54162567b65b1325da58d65b27359019da6a7721289269a'
